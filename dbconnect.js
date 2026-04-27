745942510c5190d478fb03ba249058b095ffd123
EnoahGrey <enoahgrey@gmail.com>
2026-04-26 19:12:29 -0500

    Created mock mongoDatabase. Created secret folder for connecting to that database.

---
+++ dbconnect.js
@@ -0,0 +1,14 @@
+
+// Layout - verify and adjust -Brandon
+
+const { MongoClient, ObjectId } = require('mongodb')
+const { uri } = require ('./secrets/mongodb.json')
+
+const client = new MongoClient(uri)
+
+const getCollection = async (dbName, collectionName) => {
+    await client.connect()
+    return client.db(dbName).collection(collectionName)
+}
+
+module.exports = { getCollection, ObjectId}
